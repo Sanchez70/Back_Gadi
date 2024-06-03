d6bb029da70de674007749b976bb6538168c926b
Edisson <fabianleon184@gmail.com>
2024-06-03 17:47:21 -0500

    Update import.sql

--- Back1/gadi/src/main/resources/import.sql
+++ Back1/gadi/src/main/resources/import.sql
@@ -1,2 +1,3 @@
 INSERT INTO `usuario` (`id_persona`, `id_usuario`, `contrasena`, `usuario`) VALUES (NULL, NULL, '123', 'kevin');
 INSERT INTO `usuario` (`id_persona`, `id_usuario`, `contrasena`, `usuario`) VALUES (NULL, NULL, '123', 'jean');
+INSERT INTO `usuario` (`id_persona`, `id_usuario`, `contrasena`, `usuario`) VALUES (NULL, NULL, '123', 'edisson');
