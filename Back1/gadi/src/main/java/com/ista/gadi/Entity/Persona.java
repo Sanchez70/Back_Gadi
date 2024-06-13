8ddcc3d6039786cf4677afe74f2d05b924cdc339
Santana <carsantana54@gmail.com>
2024-06-13 07:52:14 -0500

    Update Persona

--- Back1/gadi/src/main/java/com/ista/gadi/Entity/Persona.java
+++ Back1/gadi/src/main/java/com/ista/gadi/Entity/Persona.java
@@ -7,8 +7,9 @@ import java.util.List;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name="persona", uniqueConstraints= {@UniqueConstraint(columnNames= {"id_persona","cedula","id_tipo_contrato","id_titulo_profesional","id_grado_ocp"})})
-public class Persona implements Serializable{
+@Table(name = "persona", uniqueConstraints = { @UniqueConstraint(columnNames = { "id_persona", "cedula",
+		"id_tipo_contrato", "id_titulo_profesional", "id_grado_ocp" }) })
+public class Persona implements Serializable {
 
 	/**
 	 * 
@@ -16,9 +17,9 @@ public class Persona implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id_persona;
-	
+
 	private String cedula;
 	private String nombre1;
 	private String nombre2;
@@ -29,103 +30,131 @@ public class Persona implements Serializable{
 	private String correo;
 	private int edad;
 	private Date fecha_vinculacion;
-	
+
 	private Long id_tipo_contrato;
 	private Long id_titulo_profesional;
 	private Long id_grado_ocp;
-	
-	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
-	@JoinColumn(name="id_persona")
-	private List<Distributivo>Distributivo;
-	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
-	@JoinColumn(name="id_persona")
-	private List<Usuario>Usuario;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_persona")
+	private List<Distributivo> Distributivo;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_persona")
+	private List<Usuario> Usuario;
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "id_persona")
+	private List<Titulo_Profesional> titulos;
+
 	public Long getId_persona() {
 		return id_persona;
 	}
+
 	public void setId_persona(Long id_persona) {
 		this.id_persona = id_persona;
 	}
+
 	public String getCedula() {
 		return cedula;
 	}
+
 	public void setCedula(String cedula) {
 		this.cedula = cedula;
 	}
+
 	public String getNombre1() {
 		return nombre1;
 	}
+
 	public void setNombre1(String nombre1) {
 		this.nombre1 = nombre1;
 	}
+
 	public String getNombre2() {
 		return nombre2;
 	}
+
 	public void setNombre2(String nombre2) {
 		this.nombre2 = nombre2;
 	}
+
 	public String getApellido1() {
 		return apellido1;
 	}
+
 	public void setApellido1(String apellido1) {
 		this.apellido1 = apellido1;
 	}
+
 	public String getApellido2() {
 		return apellido2;
 	}
+
 	public void setApellido2(String apellido2) {
 		this.apellido2 = apellido2;
 	}
+
 	public String getTelefono() {
 		return telefono;
 	}
+
 	public void setTelefono(String telefono) {
 		this.telefono = telefono;
 	}
+
 	public String getDireccion() {
 		return direccion;
 	}
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
+
 	public String getCorreo() {
 		return correo;
 	}
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+
 	public int getEdad() {
 		return edad;
 	}
+
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
+
 	public Date getFecha_vinculacion() {
 		return fecha_vinculacion;
 	}
+
 	public void setFecha_vinculacion(Date fecha_vinculacion) {
 		this.fecha_vinculacion = fecha_vinculacion;
 	}
 
-	
 	public Long getId_titulo_profesional() {
 		return id_titulo_profesional;
 	}
+
 	public void setId_titulo_profesional(Long id_titulo_profesional) {
 		this.id_titulo_profesional = id_titulo_profesional;
 	}
+
 	public Long getId_grado_ocp() {
 		return id_grado_ocp;
 	}
+
 	public void setId_grado_ocp(Long id_grado_ocp) {
 		this.id_grado_ocp = id_grado_ocp;
 	}
+
 	public Long getId_tipo_contrato() {
 		return id_tipo_contrato;
 	}
+
 	public void setId_tipo_contrato(Long id_tipo_contrato) {
 		this.id_tipo_contrato = id_tipo_contrato;
 	}
-	
 
 }
